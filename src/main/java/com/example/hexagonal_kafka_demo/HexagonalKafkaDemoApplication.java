18a6057265f1bc070c0dbe738d7577c0ce7df4c7
Danrlei Huber <darleirafaelhuber@gmail.com>
2025-03-30 14:31:22 -0300

    first commit

---
+++ src/main/java/com/example/hexagonal_kafka_demo/HexagonalKafkaDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.hexagonal_kafka_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HexagonalKafkaDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HexagonalKafkaDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/hexagonal_kafka_demo/HexagonalKafkaDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hexagonal_kafka_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HexagonalKafkaDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
